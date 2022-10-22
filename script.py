f2d413c8971c163c9d8e2e73da6e13a493f63241
Ibha-X <89918383+Albiahbii@users.noreply.github.com>
2022-10-22 10:25:44 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,44 @@
+import os
+from datetime import datetime
+
+clear = lambda: os.system('clear')
+
+def script():
+    title = input("Enter story title: ").title()
+    characters = input("Enter comma seperated names of characters: ")
+    clear()
+    print(title.center(50))
+    characters = characters.split(",")
+    characters = [i.strip() for i in characters]
+    scene = 1
+    Plot = f"Plot - {scene}"
+    dialogues = title.center(50) + "\n" + "Characters: " + ",".join(characters) + "\n\n" + Plot
+    print(Plot)
+    run = True
+    while run:
+        for i in range(len(characters)):
+            char = f"{characters[i]}: "
+            dialogue = input(char)
+            if not dialogue:
+                dialogue = "..."
+            if dialogue == ".new":
+                scene += 1
+                print()
+                Plot = "\n" + f"Plot - {scene}"
+                print(Plot)
+                dialogues += "\n" + Plot
+                break
+            if dialogue == "x":
+                run = False
+                break
+            dialogues += "\n" + char + dialogue
+            
+    clear()
+    print(dialogues)
+    now = datetime.now()
+    now = datetime.strftime(now, "-%d-%m-%Y")
+    with open(f"scripts/{title}{now}.txt", "w") as f:
+        f.write(dialogues)
+
+script()
+
